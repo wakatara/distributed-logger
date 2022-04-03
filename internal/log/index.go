85a4caf66da6899b6122b02a3aabf79fc487d7d7
Daryl Manning <daryl@wakatara.com>
2022-04-03 22:02:30 +0800

    Fixes and tests for distributed log index.

--- internal/log/index.go
+++ internal/log/index.go
@@ -4,24 +4,24 @@ import (
 	"io"
 	"os"
 
-	"github.com/tysontate/gommap"
+	"github.com/tysonmote/gommap"
 )
 
 var (
 	offWidth uint64 = 4
 	posWidth uint64 = 8
-	entWidth = offWidth + posWidth
+	entWidth        = offWidth + posWidth
 )
 
 type index struct {
 	file *os.File
-	mmap gommap.mmap
+	mmap gommap.MMap
 	size uint64
 }
 
 func newIndex(f *os.File, c Config) (*index, error) {
 	idx := &index{
-		file f,
+		file: f,
 	}
 	fi, err := os.Stat(f.Name())
 	if err != nil {
@@ -30,21 +30,20 @@ func newIndex(f *os.File, c Config) (*index, error) {
 
 	idx.size = uint64(fi.Size())
 	if err = os.Truncate(
-		f.Name(), int64, (c.Segement.MaxIndexBytes),);
-		err != nil {
-			return nil, err
-		}
-		if idx.mmap, err = gommap.Map(
-			idx.file.Fd(),
-			gommap.PROT_READ|gommap.PROT_WRITE,
-			gommap.MAP_SHARED,
-		); err != nil, err {
-			return nil, err
-		}
-		return idx, nil
+		f.Name(), int64(c.Segment.MaxIndexBytes)); err != nil {
+		return nil, err
+	}
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+	return idx, nil
 }
 
-func (i* index) Close() error {
+func (i *index) Close() error {
 	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
 		return err
 	}
@@ -57,28 +56,28 @@ func (i* index) Close() error {
 	return i.file.Close()
 }
 
-fun (i *index) Read(i int64) (out uint32, pos uint64, err error) {
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 	if i.size == 0 {
 		return 0, 0, io.EOF
 	}
 	if in == -1 {
-		out = uint64((i.size / entWidth) - 1)
+		out = uint32((i.size / entWidth) - 1)
 	} else {
-		out = unint32(in)
+		out = uint32(in)
 	}
-	pos = uint64(out) * entWidth 
+	pos = uint64(out) * entWidth
 	if i.size < pos+entWidth {
-		return 0, 0, io.EOF 
+		return 0, 0, io.EOF
 	}
-	out = enc.Uint32(i.mmap[pos: pos+offWidth])
-	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth ])
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
 	return out, pos, nil
 }
 
 func (i *index) Write(off uint32, pos uint64) error {
-    if uint64(len(i.mmap)) < i.size+entWidth {
+	if uint64(len(i.mmap)) < i.size+entWidth {
 		return io.EOF
-	}        
+	}
 	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
 	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
 	i.size += uint64(entWidth)
@@ -88,8 +87,3 @@ func (i *index) Write(off uint32, pos uint64) error {
 func (i *index) Name() string {
 	return i.file.Name()
 }
-
-
-
-
-
